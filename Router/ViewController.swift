39b4717e87a6904d63ea10cbde76f41642c71770
Shima <shima.jin@icloud.com>
2018-09-09 17:50:46 +0900

    wip

--- Router/ViewController.swift
+++ Router/ViewController.swift
@@ -8,17 +8,48 @@
 
 import UIKit
 
-struct Path<T: UIViewController> {
-    let destination: T
-    init(destination: T) {
+
+typealias Node = UIViewController & Pathable
+
+struct Path<S: Node, D: Node> {
+
+    let source: S
+    let destination: D
+
+    var identifier: String {
+        return String(describing: type(of: source)) + "/" + String(describing: type(of: destination))
+    }
+
+    var path: Path<S, D> {
+        return self
+    }
+
+    init(source: S, destination: D) {
+        self.source = source
         self.destination = destination
     }
-}
-
-protocol Pathable {
 
 }
 
+protocol Pathable { }
+
+
+//struct Edge {
+//
+//    typealias Node = UIViewController
+//
+//    let source: Node
+//    let destination: Node
+//
+//    var identifier: String {
+//        return String(describing: type(of: source)) + "/" + String(describing: type(of: destination))
+//    }
+//
+//    var edge: Edge {
+//        return self
+//    }
+//
+//}
 
 class ViewController1: UIViewController {
 
@@ -27,17 +58,25 @@ class ViewController1: UIViewController {
 
         view.backgroundColor = .red
 
-        _ = toVC2().destination.toVC3()
+        let a = toVC2().destination.toVC3()
+        print("path identifier:", a.identifier)
 
     }
 }
 
-extension ViewController1: Pathable {
-    func toVC2() -> Path<ViewController2> {
+extension ViewController1 : Pathable {
+
+    func toVC2() -> Path<ViewController1, ViewController2> {
         let controller = ViewController2()
         navigationController?.pushViewController(controller, animated: false)
-        return Path(destination: controller)
+        return Path(source: self, destination: controller)
     }
+
+//    func toVC2() -> Edge {
+//        let controller = ViewController2()
+//        navigationController?.pushViewController(controller, animated: false)
+//        return Edge(source: self, destination: controller)
+//    }
 }
 
 
@@ -50,12 +89,19 @@ class ViewController2: UIViewController {
     }
 }
 
-extension ViewController2: Pathable {
-    func toVC3() -> Path<ViewController3>{
+extension ViewController2 : Pathable {
+
+    func toVC3() -> Path<ViewController2, ViewController3>{
         let controller = ViewController3()
         navigationController?.pushViewController(controller, animated: false)
-        return Path(destination: controller)
+        return Path(source: self, destination: controller)
     }
+
+//    func toVC3() -> Edge {
+//        let controller = ViewController3()
+//        navigationController?.pushViewController(controller, animated: true)
+//        return Edge(source: self, destination: controller)
+//    }
 }
 
 class ViewController3: UIViewController {
@@ -67,4 +113,6 @@ class ViewController3: UIViewController {
     }
 }
 
+extension ViewController3: Pathable {
 
+}
